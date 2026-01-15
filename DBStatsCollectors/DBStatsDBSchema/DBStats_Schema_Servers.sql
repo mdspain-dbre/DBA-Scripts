df093a9d1e9c8fc4083743b7e78276c13e1b1c8a
mdspain-dbre <michael.dspain@vizio.com>
2026-01-15 14:04:17 -0700

    added schema export

---
+++ DBStatsCollectors/DBStatsDBSchema/DBStats_Schema_Servers.sql
@@ -0,0 +1,7 @@
+﻿/*
+	Created by SEADATA\michael.dspain using dbatools Export-DbaScript for objects on dre-jumpbox at 01/15/2026 21:02:52
+	See https://dbatools.io/Export-DbaScript for more information
+*/
+CREATE SCHEMA [Servers]
+GO
+
